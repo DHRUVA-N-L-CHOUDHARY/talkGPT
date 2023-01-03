cabda244cc9a216c46ff4edbd9c89120fd32a2d4
Kranthiii <lci2021053@iiitl.ac.in>
2023-01-03 18:43:53 +0530

    first commit #03-01-2023

---
+++ lib/src/views/main.dart
@@ -0,0 +1,27 @@
+import 'package:cloud_project/src/views/splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+  
+        primarySwatch: Colors.orange,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
+
---
+++ lib/src/views/splash_screen.dart
@@ -0,0 +1,63 @@
+import 'dart:async';
+import 'package:cloud_project/src/views/home_page.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    
+    super.initState();
+    Timer(const Duration(seconds: 5), ()=>Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>const Home())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomRight,
+              colors: [
+                Color(0xFFFF8008),
+                Color(0xFFCE1010),
+              ],
+            ),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                children: [
+                  Image.asset(
+                    'lib/assests/images/chatt.png',
+                    height: 30,
+                    width: 30,
+                  ),
+                  const Text("Chat GPT",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18.0,
+                  ),
+                  ),
+                ],
+              ),
+              
+                  const CircularProgressIndicator(),
+            ],
+          )),
+    );
+  }
+}
