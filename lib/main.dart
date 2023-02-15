754feb9b8e79c962cf53a946cb2ae9bfedd2b14f
Kranthiii <lci2021053@iiitl.ac.in>
2023-02-15 15:13:17 +0530

    clearcode

--- lib/main.dart
+++ lib/main.dart
@@ -54,21 +54,4 @@ class _AuthenticateState extends State<Authenticate> {
 }
 
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return GetMaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-  
-//         primarySwatch: Colors.orange,
-//       ),
-//       home: const SplashScreen(),
-//     );
-//   }
-// }
 
--- lib/src/views/chat_screen.dart
+++ lib/src/views/chat_screen.dart
@@ -72,12 +72,7 @@ class _ChatScreenState extends State<ChatScreen> {
             body: Center(
               child: SingleChildScrollView(
                 child:
-                    // Padding(
-                    //   padding: EdgeInsets.all(15.0),
-                    //   child: ChatContainer(
-                    //     inputtext: controller.speechText.value,
-                    //   ),
-                    // ),
+                    
                     Obx(
                   () => controller.isListening.value
                       ? Container(
@@ -97,25 +92,34 @@ class _ChatScreenState extends State<ChatScreen> {
                           ),
                         ),
                 ),
-                // Padding(
-                //   padding: EdgeInsets.all(15.0),
-                //   child: ChatContainer(
-                //     inputtext: ans,
-                //   ),
-                // ),
-                // Obx(
-                //   () => Text(
-                //     controller.speechText.value,
-                //     style: TextStyle(
-                //       fontSize: 18,
-                //     ),
-                //   ),
-                // ),
               ),
             ),
-            floatingActionButtonLocation:
-                FloatingActionButtonLocation.centerDocked,
-            floatingActionButton: Obx(
+            // floatingActionButtonLocation:
+            //     FloatingActionButtonLocation.centerDocked,
+            // floatingActionButton: Obx(
+            //   () => AvatarGlow(
+            //     child:  FloatingActionButton(
+            //       backgroundColor: Colors.black,
+            //       child: Icon(
+            //         controller.isListening.value ? Icons.mic : Icons.mic_none,
+            //       ),
+            //       onPressed: () {
+            //         controller.listen();
+            //         setState(() {
+            //           getsearch(controller.speechText.value);
+            //           // list.add(ChatContainer(inputtext: controller.speechText.value));
+            //           controller.speechText.value = '';
+            //         });
+            //       },
+            //     ),
+            //     animate: controller.isListening.value,
+            //     endRadius: 90,
+            //     duration: Duration(milliseconds: 2000),
+            //     repeat: true,
+            //     repeatPauseDuration: Duration(milliseconds: 2000),
+            //   ),
+            // ),
+            bottomSheet: Obx(
               () => AvatarGlow(
                 child:  FloatingActionButton(
                   backgroundColor: Colors.black,
@@ -126,7 +130,6 @@ class _ChatScreenState extends State<ChatScreen> {
                     controller.listen();
                     setState(() {
                       getsearch(controller.speechText.value);
-                      // list.add(ChatContainer(inputtext: controller.speechText.value));
                       controller.speechText.value = '';
                     });
                   },
@@ -138,37 +141,6 @@ class _ChatScreenState extends State<ChatScreen> {
                 repeatPauseDuration: Duration(milliseconds: 2000),
               ),
             ),
-            // bottomSheet: Container(
-            //   color: Colors.grey.shade300,
-            //   child: Padding(
-            //     padding: const EdgeInsets.all(10.0),
-            //     child: Row(
-            //       mainAxisAlignment: MainAxisAlignment.center,
-            //       children: [
-            //         Custombutton(
-            //           text: "Submit",
-            //           inputcolor: Colors.black,
-            //           customheight: 55,
-            //           customwidth: 330,
-            //           inputrad: 15,
-            //           textStyle: const TextStyle(
-            //             fontSize: 22,
-            //             color: Colors.white,
-            //           ),
-            //           press: () {
-            //             // print(ans);
-            //             list.add(ChatContainer(inputtext: controller.speechText.value));
-            //             setState(() {
-            //               getsearch(controller.speechText.value);
-            //               // list.add(ChatContainer(inputtext: controller.speechText.value));
-            //             controller.speechText.value = '';
-            //             });
-            //           },
-            //         ),
-            //       ],
-            //     ),
-            //   ),
-            // )
           );
         });
   }
